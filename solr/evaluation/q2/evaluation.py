9707f42e482f585744b9d29e801c1b36827b06a5
Mariana Truta <marianatruta2000@gmail.com>
2021-12-13 13:07:17 +0000

    updated precision recall curves

--- solr/evaluation/q2/evaluation.py
+++ solr/evaluation/q2/evaluation.py
@@ -1,129 +1,141 @@
-import matplotlib.pyplot as plt
-from sklearn.metrics import PrecisionRecallDisplay
-import numpy as np
 import json
-import requests
-import pandas as pd
-import sys
-import os
+from itertools import cycle
 
-needsWeight = False
-if '-w' in sys.argv:
-    needsWeight = True
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+from sklearn.metrics import PrecisionRecallDisplay
 
 QRELS_FILE = "query.txt"
-QUERY_URL = "http://localhost:8983/solr/goodreads/select?defType=edismax&indent=true&q.op=AND&q=interesting%20AND%20(fiction%20OR%20romance)&qf=genre%20positive_reviews&rows=30"
-QUERY_URL_WEIGHT = "http://localhost:8983/solr/goodreads/select?defType=edismax&indent=true&q.op=AND&q=interesting%20AND%20(fiction%20OR%20romance)&qf=genre%5E5%20positive_reviews&rows=30"
+QUERY_URL = "http://localhost:8983/solr/goodreads-sample/select?defType=edismax&indent=true&q.op=OR&q=interesting%20AND%20(fiction%20OR%20romance)&qf=genre%20positive_reviews&rows=20"
+QUERY_URL_WEIGHT = "http://localhost:8983/solr/goodreads-sample/select?defType=edismax&indent=true&q.op=OR&q=interesting%20AND%20(fiction%20OR%20romance)&qf=genre%5E1.8%20positive_reviews&rows=20"
 
 # Read qrels to extract relevant documents
 relevant = list(map(lambda el: el.strip(), open(QRELS_FILE).readlines()))
 
 # Get query results from Solr instance
-if needsWeight:
-    file_obj = "query_results_weight.txt"
-    results = requests.get(QUERY_URL_WEIGHT).json()['response']['docs']
-else:
-    file_obj = "query_results.txt"
-    results = requests.get(QUERY_URL).json()['response']['docs']
+normalResults = requests.get(QUERY_URL).json()['response']['docs']
+weightResults = requests.get(QUERY_URL_WEIGHT).json()['response']['docs']
+noSchemaResults = json.load(open('noSchema.json', encoding="utf8"))[
+    'response']['docs']
 
-if os.path.exists(file_obj):
-    os.remove(file_obj)
+results = [noSchemaResults, normalResults, weightResults]
 
-file_object = open(file_obj, 'a')
+_, ax = plt.subplots(figsize=(7, 8))
 
-for book in results:
-    file_object.write(book["link"])
-    file_object.write('\n')
+colors = cycle(["darkorange", "teal",  "gold"])
 
-file_object.close()
+i = 0
+for results, color in zip(results, colors):
 
-# METRICS TABLE
-# Define custom decorator to automatically calculate metric based on key
-metrics = {}
-metric = lambda f: metrics.setdefault(f.__name__, f)
+    # METRICS TABLE
+    # Define custom decorator to automatically calculate metric based on key
+    metrics = {}
+    def metric(f): return metrics.setdefault(f.__name__, f)
 
-@metric
-def ap(results, relevant):
-    """Average Precision"""
-    precision_values = [
-        len([
-            doc 
-            for doc in results[:idx]
-            if doc['link'] in relevant
-        ]) / idx 
-        for idx in range(1, len(results))
-    ]
-    return sum(precision_values)/len(precision_values)
+    @metric
+    def ap(results, relevant):
+        """Average Precision"""
+        precision_values = [
+            len([
+                doc
+                for doc in results[:idx]
+                if (i != 0 and doc['link'] in relevant) or (i == 0 and doc['link'][0] in relevant)
+            ]) / idx
+            for idx in range(1, len(results))
+        ]
+        return sum(precision_values)/len(precision_values)
 
-@metric
-def p10(results, relevant, n=10):
-    """Precision at N"""
-    return len([doc for doc in results[:n] if doc['link'] in relevant])/n
+    @metric
+    def p10(results, relevant, n=10):
+        """Precision at N"""
+        return len([
+            doc
+            for doc in results[:n]
+            if (i != 0 and doc['link'] in relevant) or (i == 0 and doc['link'][0] in relevant)
+        ]) / n
 
-def calculate_metric(key, results, relevant):
-    return metrics[key](results, relevant)
+    def calculate_metric(key, results, relevant):
+        return metrics[key](results, relevant)
 
-# Define metrics to be calculated
-evaluation_metrics = {
-    'ap': 'Average Precision',
-    'p10': 'Precision at 10 (P@10)'
-}
+    # Define metrics to be calculated
+    evaluation_metrics = {
+        'ap': 'Average Precision',
+        'p10': 'Precision at 10 (P@10)'
+    }
 
-# Calculate all metrics and export results as LaTeX table
-df = pd.DataFrame([['Metric','Value']] +
-    [
+    # Calculate all metrics and export results as LaTeX table
+    df = pd.DataFrame([['Metric', 'Value']] +
+                      [
         [evaluation_metrics[m], calculate_metric(m, results, relevant)]
         for m in evaluation_metrics
     ]
-)
+    )
 
-if needsWeight:
-    filename = 'results_weight.tex'
-else:
-    filename = 'results.tex'
+    if i == 0:
+        filename = 'results_no_schema.tex'
+    elif i == 1:
+        filename = 'results.tex'
+    else:
+        filename = 'results_weight.tex'
 
-with open(filename,'w') as tf:
-    tf.write(df.to_latex())
+    with open(filename, 'w') as tf:
+        tf.write(df.to_latex())
 
+    # PRECISION-RECALL CURVE
+    # Calculate precision and recall values as we move down the ranked list
+    precision_values = [
+        len([
+            doc
+            for doc in results[:idx]
+            if (i != 0 and doc['link'] in relevant) or (i == 0 and doc['link'][0] in relevant)
+        ]) / idx
+        for idx, _ in enumerate(results, start=1)
+    ]
 
-# PRECISION-RECALL CURVE
-# Calculate precision and recall values as we move down the ranked list
-precision_values = [
-    len([
-        doc 
-        for doc in results[:idx]
-        if doc['link'] in relevant
-    ]) / idx 
-    for idx, _ in enumerate(results, start=1)
-]
+    recall_values = [
+        len([
+            doc for doc in results[:idx]
+            if (i != 0 and doc['link'] in relevant) or (i == 0 and doc['link'][0] in relevant)
+        ]) / len(relevant)
+        for idx, _ in enumerate(results, start=1)
+    ]
 
-recall_values = [
-    len([
-        doc for doc in results[:idx]
-        if doc['link'] in relevant
-    ]) / len(relevant)
-    for idx, _ in enumerate(results, start=1)
-]
+    precision_recall_match = {k: v for k,
+                              v in zip(recall_values, precision_values)}
 
-precision_recall_match = {k: v for k,v in zip(recall_values, precision_values)}
+    # Extend recall_values to include traditional steps for a better curve (0.1, 0.2 ...)
+    recall_values.extend([step for step in np.arange(
+        0.1, 1.1, 0.1) if step not in recall_values])
+    recall_values = sorted(set(recall_values))
 
-# Extend recall_values to include traditional steps for a better curve (0.1, 0.2 ...)
-recall_values.extend([step for step in np.arange(0.1, 1.1, 0.1) if step not in recall_values])
-recall_values = sorted(set(recall_values))
+    # Extend matching dict to include these new intermediate steps
+    for idx, step in enumerate(recall_values):
+        if step not in precision_recall_match:
+            if recall_values[idx-1] in precision_recall_match:
+                precision_recall_match[step] = precision_recall_match[recall_values[idx-1]]
+            else:
+                precision_recall_match[step] = precision_recall_match[recall_values[idx+1]]
 
-# Extend matching dict to include these new intermediate steps
-for idx, step in enumerate(recall_values):
-    if step not in precision_recall_match:
-        if recall_values[idx-1] in precision_recall_match:
-            precision_recall_match[step] = precision_recall_match[recall_values[idx-1]]
-        else:
-            precision_recall_match[step] = precision_recall_match[recall_values[idx+1]]
+    disp = PrecisionRecallDisplay(
+        [precision_recall_match.get(r) for r in recall_values], recall_values)
+    if(i == 0):
+        disp.plot(ax=ax, name=f"Precision-recall without schema",
+                  color=color, linewidth=1)
+    elif(i == 1):
+        disp.plot(ax=ax, name=f"Precision-recall without weights",
+                  color=color, linewidth=1)
+    elif(i == 2):
+        disp.plot(ax=ax, name=f"Precision-recall with weights",
+                  color=color, linewidth=1.5)
+    i += 1
 
-disp = PrecisionRecallDisplay([precision_recall_match.get(r) for r in recall_values], recall_values)
-disp.plot()
-plt.ylim((0, 1.1))
+# add the legend for the iso-f1 curves
+handles, labels = disp.ax_.get_legend_handles_labels()
 
-if needsWeight:
-    plt.savefig('precision_recall_weight.pdf')
-else:
-    plt.savefig('precision_recall.pdf')
+# set the legend and the axes
+ax.legend(handles=handles, labels=labels, loc="best")
+ax.set_title("Precision-Recall curve of Query 1")
+
+plt.savefig('precision_recall.png')
--- solr/startup.sh
+++ solr/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 precreate-core goodreads
+precreate-core goodreads-sample
 
 # Start Solr in background mode so we can use the API to upload the schema
 solr start
@@ -12,8 +13,15 @@ curl -X POST -H 'Content-type:application/json' \
     --data-binary @/data/schema.json \
     http://localhost:8983/solr/goodreads/schema
 
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary @/data/schema.json \
+    http://localhost:8983/solr/goodreads-sample/schema
+
 # Populate collection
 bin/post -c goodreads /data/data.json
+
+# Populate collection
+bin/post -c goodreads-sample /data/data_sample.json
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
