9f965e79b7ca9e9909fdb221191122cf22cc98dd
Mariana Truta <marianatruta2000@gmail.com>
2021-12-22 11:43:24 +0000

    added readme

---
+++ milestone3/README.md
@@ -0,0 +1,4 @@
+# Install dependencies and tun project
+
+Inside each folder, execute the following commands:
+`npm install` and `npm start`.
--- milestone3/backend/controllers/index.js
+++ milestone3/backend/controllers/index.js
@@ -1,5 +1,4 @@
 async function test(req, res) {
-    console.log("oi");
     return res.status(200).json({ message: 'Hello from Backend' });
 }
 
